0b18e1315f751cbb9faf2b7ce58f846b504a0d34
= <=>
2022-12-10 20:04:18 +0530

    feat: Build basic landing page to consume GraphQL API

---
+++ src/pages/tracks.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import { Layout } from '../components';
+import {gql, useQuery} from "@apollo/client"
+import TrackCard from '../containers/track-card'
+
+const TRACKS = gql`
+    query GetTracks{
+        tracksForHome {
+            id
+            title
+            author {
+                id
+                name
+                photo
+            }
+            thumbnail
+            length
+            modulesCount
+        }
+    }
+`
+
+/**
+ * Tracks Page is the Catstronauts home page.
+ * We display a grid of tracks fetched with useQuery with the TRACKS query
+ */
+const Tracks = () => {
+  const {loading, error, data} = useQuery(TRACKS)
+
+  if(loading) return 'Loading....'
+
+  if(error) return `ERROR ${error.message}`
+
+  console.log(data)
+  return <Layout grid>{data?.tracksForHome?.map(track => (
+    <TrackCard key={track.id} track={track}/>
+  ))}</Layout>;
+};
+
+export default Tracks;
